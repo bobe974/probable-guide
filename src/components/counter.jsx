65786fc1858e037503da29fcfaa9d66ff8b49c2f
BAILLIF Etienne <etienne.baillif@humansconnexion.com>
2025-07-10 15:52:51 +0200

    c encore +  bo

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -22,10 +22,13 @@ class Counter extends Component {
   render() {
     console.log(this.props);
     return (
-      <React.Fragment>
-        <span style={this.styles} className={this.getBadgeClasses()}>
+      <div className="row">
+        <div className="col-1">
+                  <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
+          <div className="col"></div>
+        </div>
         <button
           onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btm-sm m-2"
@@ -45,19 +48,19 @@ class Counter extends Component {
         >
           X
         </button>
-      </React.Fragment>
+      </div>
     );
   }
 
   getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    let classes = "badge badge-";
+    classes += this.props.counter.value === 0 ? "warning " : "primary";
     return classes;
   }
 
   formatCount() {
     const value = this.props.counter.value;
-    return value === 0 ? <p>zero</p> : value;
+    return value === 0 ? 0 : value;
   }
 }
 
